9c60dfc7273df55c67b1d8f2fae2dc44fa3aef71
dshah374@gatech.edu <90000344+DhruvShah09@users.noreply.github.com>
2023-05-10 20:40:48 -0400

    cleaned cclinks

---
+++ event_cleaner.py
@@ -0,0 +1,7 @@
+with open("cclinks.txt", "r") as input_file:
+    lines = input_file.readlines()
+
+with open("cclinksnoevent.txt", "w") as output_file:
+    for line in lines:
+        if "https://www.cc.gatech.edu/events/" not in line:
+            output_file.write(line)
