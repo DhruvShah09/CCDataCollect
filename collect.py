5d1bba166a38accfc33ea847a10a8f2e3378fa1e
dshah374@gatech.edu <90000344+DhruvShah09@users.noreply.github.com>
2023-05-10 20:11:56 -0400

    fixed bug introduced by bug fix

--- collect.py
+++ collect.py
@@ -6,7 +6,7 @@ out_file = open("out.txt", "a")
 for i in range(len(links)): 
     print(i + 1) 
     result = extract(fetch_url(links[i].strip()))
-    if (result is none):
+    if (result is None):
         print("error")
         continue
     out_file.write(result +"\n")
