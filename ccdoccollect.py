a597897e509ec1ddaf0ef8ff816bdff1a245f29f
dshah374@gatech.edu <90000344+DhruvShah09@users.noreply.github.com>
2023-05-11 17:33:07 -0400

    added pagination for coc text collection

---
+++ ccdoccollect.py
@@ -0,0 +1,20 @@
+from trafilatura import fetch_url, extract 
+
+link_file = open("cclinks.txt", "r")
+links = link_file.readlines()
+z = 0
+
+for i in range(len(links)): 
+    print(i + 1) 
+    outdir = "outdir/" 
+    write_addr = "doc" + z + ".txt"
+    addr = outdir + write_addr
+    z += 1
+    outfile = open(addr , "a")
+    url = links[i].strip()
+    result = extract(fetch_url(url))
+    if (result is None):
+        print("error")
+        continue
+    out_file.write(result +"\n")
+    print("done")
