a412937cacc4ecefc8e9cc0ccc2d97fc17df3c45
dshah374@gatech.edu <90000344+DhruvShah09@users.noreply.github.com>
2023-05-10 20:32:23 -0400

    Links changed

--- collect.py
+++ collect.py
@@ -1,8 +1,8 @@
 from trafilatura import fetch_url, extract 
 
-link_file = open("clinks.txt", "r")
+link_file = open("cclinks.txt", "r")
 links = link_file.readlines()
-out_file = open("out.txt", "a")
+out_file = open("ccout.txt", "a")
 for i in range(len(links)): 
     print(i + 1) 
     result = extract(fetch_url(links[i].strip()))
